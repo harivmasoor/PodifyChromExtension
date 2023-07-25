665b501fc44233076152a9a670a69c1d1ec655a2
harivmasoor <harimasoor@gmail.com>
2023-07-25 15:13:34 -0700

    push

--- background.js
+++ background.js
@@ -1,48 +1,52 @@
 const targetOrigin = "https://harivmasoor.github.io";
 
-chrome.runtime.onMessage.addListener((request, sender) => {
-  if (request.action === 'captureTabAudio' && sender.origin === targetOrigin) {
-    chrome.tabCapture.capture({
-      audio: true,
-      video: false
-    }, (stream) => {
-      if (chrome.runtime.lastError) {
-        console.error(chrome.runtime.lastError);
-        return;
-      }
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === 'captureTabAudio' && sender.origin === targetOrigin) {
+        chrome.tabCapture.capture({
+            audio: true,
+            video: false
+        }, (stream) => {
+            if (chrome.runtime.lastError) {
+                console.error(chrome.runtime.lastError);
+                sendResponse({error: chrome.runtime.lastError.message});
+                return;
+            }
 
-      const mediaRecorder = new MediaRecorder(stream);
-      let audioChunks = [];
+            const mediaRecorder = new MediaRecorder(stream);
+            let audioChunks = [];
 
-      mediaRecorder.ondataavailable = (event) => {
-        audioChunks.push(event.data);
-      };
+            mediaRecorder.ondataavailable = (event) => {
+                audioChunks.push(event.data);
+            };
 
-      mediaRecorder.onstop = async () => {
-        const audioBlob = new Blob(audioChunks, { type: 'audio/webm' });
-        
-        try {
-          const response = await chrome.tabs.sendMessage(sender.tabId, {
-            audioBlob: audioBlob
-          });
+            mediaRecorder.onstop = () => {
+                const audioBlob = new Blob(audioChunks, { type: 'audio/webm' });
+                
+                chrome.tabs.sendMessage(sender.tab.id, {
+                    audioBlob: audioBlob
+                }, response => {
+                    if(chrome.runtime.lastError) {
+                        console.error("Failed to send blob:", chrome.runtime.lastError);
+                    } else {
+                        console.log("Content script received blob:", response);
+                    }
+                });
 
-          console.log("Content script received blob");
-        } catch(error) {
-          console.error("Failed to send blob:", error);
-        }
+                stopStream(stream);
+            };
 
-        stopStream(stream);
-      };
+            mediaRecorder.start();
+            setTimeout(() => mediaRecorder.stop(), 5000); // Recording for 5 seconds for demonstration
+        });
 
-      mediaRecorder.start();
-      setTimeout(() => mediaRecorder.stop(), 5000); // Recording for 5 seconds for demonstration
-    });
-    return true;
-  }
+        // Indicate we wish to send a response asynchronously
+        return true;
+    }
 });
 
 function stopStream(stream) {
-  if (stream && stream.getTracks) {
-    stream.getTracks().forEach(track => track.stop());
-  }
+    if (stream && stream.getTracks) {
+        stream.getTracks().forEach(track => track.stop());
+    }
 }
+
