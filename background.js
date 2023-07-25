145153b8bfbfd908f07e2b37e9fc0f55c2d568ed
harivmasoor <harimasoor@gmail.com>
2023-07-25 15:03:39 -0700

    push

--- background.js
+++ background.js
@@ -1,46 +1,47 @@
-// Target origin for tabCapture
 const targetOrigin = "https://harivmasoor.github.io";
 
 chrome.runtime.onMessage.addListener((request, sender) => {
-
-  if (request.action === 'captureTabAudio' && 
-      sender.origin === targetOrigin) {
-
+  if (request.action === 'captureTabAudio' && sender.origin === targetOrigin) {
     chrome.tabCapture.capture({
       audio: true,
-      video: false,
-      tabId: sender.tabId
+      video: false
     }, (stream) => {
-
       if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError);
         return;
       }
 
-      // Create audio Blob from stream
-      const audioBlob = new Blob([stream], {type: 'audio/webm'});
-      
-      // Send to content script
-      chrome.tabs.sendMessage(sender.tabId, {
-        audioBlob: audioBlob
-      });
+      const mediaRecorder = new MediaRecorder(stream);
+      let audioChunks = [];
 
-      stopStream(stream.id);
+      mediaRecorder.ondataavailable = (event) => {
+        audioChunks.push(event.data);
+      };
 
+      mediaRecorder.onstop = async () => {
+        const audioBlob = new Blob(audioChunks, { type: 'audio/webm' });
+        
+        try {
+          const response = await chrome.tabs.sendMessage(sender.tabId, {
+            audioBlob: audioBlob
+          });
+
+          console.log("Content script received blob");
+        } catch(error) {
+          console.error("Failed to send blob:", error);
+        }
+
+        stopStream(stream);
+      };
+
+      mediaRecorder.start();
+      setTimeout(() => mediaRecorder.stop(), 5000); // Recording for 5 seconds for demonstration
     });
-
   }
-
 });
 
-
-function stopStream(streamId) {
-
-  chrome.tabCapture.stop(streamId, () => {
-
-    if (chrome.runtime.lastError) {
-      console.error(chrome.runtime.lastError);
-    }
-
-  });
-
+function stopStream(stream) {
+  if (stream && stream.getTracks) {
+    stream.getTracks().forEach(track => track.stop());
+  }
 }
--- contentScript.js
+++ contentScript.js
@@ -1,14 +1,19 @@
-chrome.runtime.onMessage.addListener(msg => {
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  if (msg.audioBlob) {
+      downloadAudio(msg.audioBlob);
+      sendResponse("Download triggered");
+  }
+  // Indicate we wish to send a response asynchronously
+  return true;
+});
 
-    if(msg.audioBlob) {
-  
-      const url = URL.createObjectURL(msg.audioBlob);
-      const a = document.createElement('a');
-      a.href = url;
-      a.download = 'tab-audio.webm'; // File name
-      a.click();
-      URL.revokeObjectURL(url);
-  
-    }
-  
-  })
+function downloadAudio(audioBlob) {
+  const url = URL.createObjectURL(audioBlob);
+
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'tab-audio.webm';
+  a.click();
+
+  URL.revokeObjectURL(url);
+}
