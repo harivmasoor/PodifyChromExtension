8986d4b7bd249a0ec1dfa49143bb5a7b79a248f9
harivmasoor <harimasoor@gmail.com>
2023-07-25 14:41:12 -0700

    push

--- background.js
+++ background.js
@@ -1,27 +1,44 @@
+// Target origin to allow tabCapture from
+const targetOrigin = "https://harivmasoor.github.io"; 
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === 'captureTabAudio') {
-        chrome.tabCapture.capture({
-            audio: true,
-            video: false
-        }, (stream) => {
-            // Check if there's an error.
-            if (chrome.runtime.lastError) {
-                console.error(chrome.runtime.lastError);
-                sendResponse({ error: chrome.runtime.lastError.message });
-                return;
-            }
 
-            // You now have the tab's audio stream.
-            // You can pass this stream to your web app or handle it here as needed.
-            if (stream && stream.id) {
-                sendResponse({ streamId: stream.id });
-            } else {
-                sendResponse({ error: 'Failed to capture audio.' });
-            }
-        });
+  if(request.action === 'captureTabAudio' && sender.origin === targetOrigin) {
 
-        return true;  // This should be kept to handle async events in MV2 but might not be effective in MV3.
-    }
+    chrome.tabCapture.capture({
+      audio: true,  
+      video: false,
+      tabId: sender.tabId
+    }, (stream) => {
+
+      if(chrome.runtime.lastError) {
+        return; 
+      }
+
+      chrome.tabs.sendMessage(sender.tabId, {
+        streamId: stream.id  
+      });
+
+      stopStream(stream.id);
+
+    });
+
+  }
+
 });
+
+
+function stopStream(streamId) {
+
+  // Stop the stream when done
+  chrome.tabCapture.stop(streamId, () => {
+    
+    if(chrome.runtime.lastError) {
+      console.error(chrome.runtime.lastError); 
+    }
+
+  });
+
+}
 
 
--- popup.js
+++ popup.js
@@ -1,20 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
+
     const startCaptureButton = document.getElementById('startCapture');
-
-    if (startCaptureButton) {
-        startCaptureButton.addEventListener('click', () => {
-            chrome.runtime.sendMessage({ action: 'captureTabAudio' }, (response) => {
-                if (response.error) {
-                    console.error('Error capturing audio:', response.error);
-                    return;
-                }
-                
-                console.log(response.streamId);
-            });
-        });
-    } else {
-        console.error('startCapture button not found!');
+    const statusDiv = document.getElementById('status');
+  
+    if(!startCaptureButton) {
+      statusDiv.textContent = 'Error: Button not found';
+      return;
     }
-});
-
+  
+    startCaptureButton.addEventListener('click', () => {
+      
+      statusDiv.textContent = 'Capturing...';
+  
+      chrome.runtime.sendMessage({action: 'captureTabAudio'}, (response) => {
+        
+        if(chrome.runtime.lastError) {
+          statusDiv.textContent = chrome.runtime.lastError.message;
+          return;
+        }
+  
+        statusDiv.textContent = 'Success!';  
+        window.close();
+        
+      });
+  
+    });
+  
+  });
 
